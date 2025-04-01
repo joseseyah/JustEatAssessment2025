3ba7f47761ac83e0a28f64c5425e0a101e4efba8
Joseph Hayes <joseph.hayes003@gmail.com>
2025-04-01 08:25:23 +0100

    feat: Results shows resurants not filtered

--- JustEat/JustEat/Views/Compondents/ResultsView.swift
+++ JustEat/JustEat/Views/Compondents/ResultsView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct ResultsView: View {
     let selectedCategory: String
 
+    @StateObject private var viewModel = RestaurantViewModel()
+    @AppStorage("userPostcode") private var postcode: String = ""
+    @State private var hasLoaded = false
+
     var body: some View {
         ZStack {
             LinearGradient(
@@ -35,9 +39,35 @@ struct ResultsView: View {
                     .shadow(color: .black.opacity(0.25), radius: 5, x: 0, y: 4)
 
                 Spacer()
+
+                if viewModel.restaurants.isEmpty {
+                    Spacer()
+                    ProgressView("Loading restaurants...")
+                        .progressViewStyle(CircularProgressViewStyle(tint: .white))
+                        .foregroundColor(.white)
+                    Spacer()
+                } else {
+                    ScrollView {
+                        LazyVStack(spacing: 16) {
+                            ForEach(viewModel.restaurants) { restaurant in
+                                RestaurantCardView(restaurant: restaurant)
+                                    .padding(.horizontal)
+                            }
+                        }
+                        .padding(.top)
+                    }
+                }
+
+
             }
             .frame(maxWidth: .infinity)
             .padding()
+            .onAppear{
+              if !hasLoaded && !postcode.isEmpty{
+                viewModel.fetchRestaurants(postcode: postcode)
+                hasLoaded = true
+              }
+            }
         }
     }
 }
